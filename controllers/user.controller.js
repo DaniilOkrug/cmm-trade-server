b41ea82ac026ee3c7c5b5d811ecef8c26c8c0776
Daniil <daniil.okrug@gmail.com>
2022-01-29 17:26:33 +0300

    cors fixes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -24,6 +24,7 @@ class UserController {
     async login(req, res, next) {
         try {
             const {email, password} = req.body;
+            console.log(req.body);
             const userData = await userService.login(email, password);
 
             res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
