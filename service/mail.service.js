0054513acdb1fb55d61a5e3d321cc2c00ae43071
Daniil <daniil.okrug@gmail.com>
2022-05-25 12:35:41 +0300

    Email letter change

--- service/mail.service.js
+++ service/mail.service.js
@@ -20,7 +20,7 @@ class MailService {
         await this.transporter.sendMail({
             from: process.env.SMTP_USER,
             to,
-            subject: 'Активация аккаунта на ' + process.env.API_URL,
+            subject: 'Активация аккаунта почты cmmtrade.ru',
             text: '',
             html:
                 `
